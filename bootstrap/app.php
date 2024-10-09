22b32d99daedac1efee21df9f610c03138011133
Paul Rijke <paul@ibuildings.nl>
2024-10-09 13:36:43 +0200

    Fix to handle PostSize... explorer possible disadvantages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,22 +3,33 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Http\Middleware\ValidatePostSize;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Symfony\Component\HttpFoundation\Response;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->remove(ValidatePostSize::class);
+        $middleware->web([ValidatePostSize::class]);
+        $middleware->priority([
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            ValidatePostSize::class,
+            ValidateCsrfToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response) {
             if ($response->getStatusCode() === 413) {
-                return back()->with([
-                    'message' => 'The page expired, please try again.',
+                return back()->withErrors([
+                    'message' => 'Upload too big',
                 ]);
             }
 
@@ -26,3 +37,5 @@ return Application::configure(basePath: dirname(__DIR__))
         });
         //
     })->create();
+
+return $app;
